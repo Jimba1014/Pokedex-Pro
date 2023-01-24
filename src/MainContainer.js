99681e4d571e68ed17cc2ebb6327edd70abc8dc0
Jim <jimba.ed@outlook.com>
2023-01-24 15:28:02 -0500

    added in Nav, and basic cards for pokemon

---
+++ src/MainContainer.js
@@ -0,0 +1,12 @@
+import PokeCard from "./pokeCard"
+
+function MainContainer(){
+    return(
+        <div>
+            MainPokeContainer
+            <PokeCard/>
+        </div>
+    )
+}
+
+export default MainContainer
