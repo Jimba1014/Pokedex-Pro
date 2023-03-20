d0fc0ef932e8ff911d2ca5c90066986a93a885f7
Jim <jimba.ed@outlook.com>
2023-03-20 13:12:00 -0400

    sprite piactures now show up

--- src/pokeCard.js
+++ src/pokeCard.js
@@ -1,12 +1,22 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import {useEffect, useState} from "react";
 
 function PokeCard(singlePoke){
 
+    const [poke, setPoke] = useState([])
+
+    useEffect(() => {
+        fetch(singlePoke.singlePoke.url)
+        .then((res) => res.json())
+        .then((data) => setPoke(data))
+      }, [])
+
     console.log(singlePoke)
+    console.log(poke)
     return(
         <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
+            <Card.Img variant="top" src={poke.sprites.front_default} />
             <Card.Body>
                 <Card.Title>{singlePoke.singlePoke.name}</Card.Title>
                 <Card.Text>
